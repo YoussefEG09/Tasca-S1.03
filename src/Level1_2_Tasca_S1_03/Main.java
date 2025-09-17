a958539711cc77cf2b413707307eabc14f07c05d
Youssef <youssef.eg09@gmail.com>
2025-09-17 09:52:14 +0200

    feat : finished exercise 2 of level 1

--- src/Level1_2_Tasca_S1_03/Main.java
+++ src/Level1_2_Tasca_S1_03/Main.java
@@ -3,6 +3,7 @@ package Level1_2_Tasca_S1_03;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,18 +16,17 @@ public class Main {
 
         System.out.println(numbers);
 
-        /*List<Integer> numbers2 = new ArrayList<>();
-        for (int i = 10; i >= 0; i--){
-            numbers2.add(i);
+        List<Integer> numbers2 = new ArrayList<>();
+
+        ListIterator<Integer> iterator = numbers.listIterator(numbers.size());
+
+        while (iterator.hasPrevious()) {
+            Integer integer = iterator.previous();
+            numbers2.add(integer);
+
         }
 
-        System.out.println(numbers2);*/
-
-
-        Iterator<Integer> iterator = numbers.iterator();
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
-        }
-
+        System.out.println(numbers2);
     }
 }
+
